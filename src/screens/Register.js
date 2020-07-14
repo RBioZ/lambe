7fa908f9f1ef3d6d0bc63089220bb7147cd63d16
RBioZ <jonatha_rihan@hotmail.com>
2020-07-14 08:25:54 -0300

    Register

--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,63 +1,91 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import {
-  View,
-  Text,
-  StyleSheet,
-  TouchableOpacity,
-  TextInput
-} from 'react-native';
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    TextInput
+} from 'react-native'
+import { connect } from 'react-redux'
+import { createUser } from '../store/actions/user'
 
-class Register extends Component{
-  state = {
-    name: '',
-    email:'',
-    password:''
-  }
+class Register extends Component {
+    state = {
+        name: '',
+        email: '',
+        password: ''
+    }
 
-  render(){
-    return(
-      <View style={styles.container}>
-        <TextInput placeholder='Nome' style={styles.input}
-          autoFocus={true} value={this.state.name}
-          onChangeText={name => this.setState({name})} />
-        <TextInput placeholder='Email' style={styles.input}
-          keyBoardType='email-address' value={this.state.email}
-          onChangeText={email => this.setState({email})} />
-        <TextInput placeholder='Senha' style={styles.input}
-          secureTextEntry={true} value={this.state.password}
-          onChangeText={password => this.setState({password})} />
-        <TouchableOpacity onPress={() => {}} style={styles.buttom}>
-          <Text style={styles.buttomText}>Salvar</Text>
-        </TouchableOpacity>
-      </View>
-    );
-  }
+    componentDidUpdate = prevProps => {
+        if (prevProps.isLoading && !this.props.isLoading) {
+            this.setState({
+                name: '',
+                email: '',
+                password: ''
+            })
+            this.props.navigation.navigate('Profile') // Feed
+        }
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <TextInput placeholder='Nome' style={styles.input}
+                    autoFocus={true} value={this.state.name}
+                    onChangeText={name => this.setState({ name })} />
+                <TextInput placeholder='Email' style={styles.input}
+                    keyboardType='email-address' value={this.state.email}
+                    onChangeText={email => this.setState({ email })} />
+                <TextInput placeholder='Senha' style={styles.input}
+                    secureTextEntry={true} value={this.state.password}
+                    onChangeText={password => this.setState({ password })} />
+                <TouchableOpacity 
+                    onPress={() => { this.props.onCreateUser(this.state) }} 
+                    style={styles.buttom}>
+                    <Text style={styles.buttomText}>Salvar</Text>
+                </TouchableOpacity>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    alignItems:'center',
-    justifyContent:'center'
-  },
-  buttom:{
-    marginTop:30,
-    padding:10,
-    backgroundColor: '#4286f4'
-  },
-  buttomText:{
-    fontSize:20,
-    color:'#FFF'
-  },
-  input:{
-    marginTop:20,
-    width:'90%',
-    backgroundColor:'#EEE',
-    height:40,
-    borderWidth:1,
-    borderColor:'#333',
-    paddingLeft:15
-  }
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    buttom: {
+        marginTop: 30,
+        padding: 10,
+        backgroundColor: '#4286f4'
+    },
+    buttomText: {
+        fontSize: 20,
+        color: '#FFF'
+    },
+    input: {
+        marginTop: 20,
+        width: '90%',
+        backgroundColor: '#EEE',
+        height: 40,
+        borderWidth: 1,
+        borderColor: '#333',
+        paddingLeft: 15
+    }
 })
 
-export default Register;
+const mapStateToProps = ({ user }) => {
+    return {
+        isLoading: user.isLoading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onCreateUser: user => dispatch(createUser(user))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Register)
+// export default Register
