97527f7bf1e94e9d960fa404565c3b3f716e88b7
RBioZ <jonatha_rihan@hotmail.com>
2020-07-14 20:49:24 -0300

    Final

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,7 +22,7 @@ class Header extends Component{
       <View style={styles.container}>
         <View style={styles.rowContainer}>
           <Image source={icon} style={styles.image} />
-          <Text style={styles.title}>Shared</Text>
+          <Text style={styles.title}>Shared </Text>
         </View>
         <View style={styles.userContainer}>
           <Text style={styles.user}>{name}</Text>
--- src/store/actions/user.js
+++ src/store/actions/user.js
@@ -1,8 +1,10 @@
+//'AIzaSyBfNHxUKjR01IkfsirPSVAYZO8XYLear4c'
+
 import {
-  USER_LOGGED_IN,
-  USER_LOGGED_OUT,
-  LOADING_USER,
-  USER_LOADED
+    USER_LOGGED_IN,
+    USER_LOGGED_OUT,
+    LOADING_USER,
+    USER_LOADED
 } from './actionTypes'
 import axios from 'axios'
 import { setMessage } from './message'
@@ -11,94 +13,94 @@ const authBaseURL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty'
 const API_KEY = 'AIzaSyBfNHxUKjR01IkfsirPSVAYZO8XYLear4c'
 
 export const userLogged = user => {
-  return {
-      type: USER_LOGGED_IN,
-      payload: user
-  }
+    return {
+        type: USER_LOGGED_IN,
+        payload: user
+    }
 }
 
 export const logout = () => {
-  return {
-      type: USER_LOGGED_OUT
-  }
+    return {
+        type: USER_LOGGED_OUT
+    }
 }
 
 export const createUser = user => {
-  return dispatch => {
-      dispatch(loadingUser())
-      axios.post(`${authBaseURL}/signupNewUser?key=${API_KEY}`, {
-          email: user.email,
-          password: user.password,
-          returnSecureToken: true
-      })
-          .catch(err => {
-              dispatch(setMessage({
-                  title: 'Erro',
-                  text: 'Ocorreu um erro inesperado!'
-              }))
-          })
-          .then(res => {
-              if (res.data.localId) {
-                  axios.put(`/users/${res.data.localId}.json`, {
-                      name: user.name
-                  })
-                      .catch(err => {
-                          dispatch(setMessage({
-                              title: 'Erro',
-                              text: 'Ocorreu um erro inesperado!'
-                          }))
-                      })
-                      .then(() => {
-                          dispatch(login(user))
-                      })
-              }
-          })
-  }
+    return dispatch => {
+        dispatch(loadingUser())
+        axios.post(`${authBaseURL}/signupNewUser?key=${API_KEY}`, {
+            email: user.email,
+            password: user.password,
+            returnSecureToken: true
+        })
+            .catch(err => {
+                dispatch(setMessage({
+                    title: 'Erro',
+                    text: 'Ocorreu um erro inesperado!'
+                }))
+            })
+            .then(res => {
+                if (res.data.localId) {
+                    axios.put(`/users/${res.data.localId}.json`, {
+                        name: user.name
+                    })
+                        .catch(err => {
+                            dispatch(setMessage({
+                                title: 'Erro',
+                                text: 'Ocorreu um erro inesperado!'
+                            }))
+                        })
+                        .then(() => {
+                            dispatch(login(user))
+                        })
+                }
+            })
+    }
 }
 
 export const loadingUser = () => {
-  return {
-      type: LOADING_USER
-  }
+    return {
+        type: LOADING_USER
+    }
 }
 
 export const userLoaded = () => {
-  return {
-      type: USER_LOADED
-  }
+    return {
+        type: USER_LOADED
+    }
 }
 
 export const login = user => {
-  return dispatch => {
-      dispatch(loadingUser())
-      axios.post(`${authBaseURL}/verifyPassword?key=${API_KEY}`, {
-          email: user.email,
-          password: user.password,
-          returnSecureToken: true
-      })
-          .then(res => {
-              if (res.data.localId) {
-                  user.token = res.data.idToken
-                  axios.get(`/users/${res.data.localId}.json`)
-                      .catch(err => {
-                          dispatch(setMessage({
-                              title: 'Erro',
-                              text: 'Ocorreu um erro inesperado!'
-                          }))
-                      })
-                      .then(res => {
-                          delete user.password
-                          user.name = res.data.name
-                          dispatch(userLogged(user))
-                          dispatch(userLoaded())
-                      })
-              }
-          })
-          .catch(err => {
-            dispatch(setMessage({
-                title: 'Erro',
-                text: 'Ocorreu um erro inesperado!'
-            }))
+    return dispatch => {
+        dispatch(loadingUser())
+        axios.post(`${authBaseURL}/verifyPassword?key=${API_KEY}`, {
+            email: user.email,
+            password: user.password,
+            returnSecureToken: true
         })
-  }
+            .then(res => {
+                if (res.data.localId) {
+                    user.token = res.data.idToken
+                    axios.get(`/users/${res.data.localId}.json`)
+                        .catch(err => {
+                            dispatch(setMessage({
+                                title: 'Erro',
+                                text: 'Ocorreu um erro inesperado!'
+                            }))
+                        })
+                        .then(res => {
+                            delete user.password
+                            user.name = res.data.name
+                            dispatch(userLogged(user))
+                            dispatch(userLoaded())
+                        })
+                }
+            })
+            .catch(err => {
+                dispatch(setMessage({
+                    title: 'Erro',
+                    text: 'Ocorreu um erro inesperado!'
+                }))
+            })
+    }
 }
